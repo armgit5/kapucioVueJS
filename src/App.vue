91bdcb4a3ef4035cff6721320f0441a420a07aca
Sarawuth <sarawuth@ampostech.com>
2017-09-10 11:25:07 +0700

    Add card component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
   <div id="app">
-    <kapucio-header></kapucio-header>
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-  import Header from './components/shared/Header.vue'
+import Header from '@/components/shared/Header'
 
-  export default {
-    components: {
-      'kapucio-header': Header
-    }
+export default {
+  components: {
+    'app-header': Header
   }
+}
 </script>
 
 <style>
+body {
+  background-color: #eaebec;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
